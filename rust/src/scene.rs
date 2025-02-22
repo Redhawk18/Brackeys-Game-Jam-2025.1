53df17ef38743dac5deaaa1a87c7627003d75ead
Redhawk18 <redhawk76767676@gmail.com>
2025-02-22 16:17:55 -0500

    music working

--- rust/src/scene.rs
+++ rust/src/scene.rs
@@ -1,9 +1,12 @@
+use std::default;
+
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Node3D)]
 pub struct Scene {
     base: Base<Node3D>,
+    music: Option<Gd<AudioStreamPlayer>>,
 }
 
 #[godot_api]
@@ -12,10 +15,10 @@ impl Scene {}
 #[godot_api]
 impl INode3D for Scene {
     fn init(base: Base<Node3D>) -> Self {
-        Scene { base }
+        Scene { base, music: None }
     }
 
     fn ready(&mut self) {
-        // self.music = Some(self.base().get_node_as("Music"));
+        self.music = Some(self.base().get_node_as("Music"));
     }
 }
